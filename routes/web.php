6931c348848851468bcf4749bd48932ac9fe89d0
Raemyn <zhekalah@gmail.com>
2025-09-04 12:47:15 +0500

    Создание роутов

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LeadController;
 
+// Главная страница
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Публичная форма заявки
+Route::get('/lead-form', [LeadController::class, 'create'])->name('lead.create');
+
+// Мини-админка (требует токен)
+Route::get('/admin', [LeadController::class, 'admin'])->name('admin.index');
